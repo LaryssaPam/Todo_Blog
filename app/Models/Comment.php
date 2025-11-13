84902b5e1cc43d208d9dc51e94fad557d864ad5e
LaryssaPam <laryssapam6@gmail.com>
2025-11-13 11:35:11 -0400

    first commit

---
+++ app/Models/Comment.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Comment extends Model
+{
+    // Ajouter HasFactory et impoter
+    use HasFactory;
+     protected $fillable =[
+        'message',
+        'state',
+        'user_id',
+        'post_id',
+        
+     ];
+     protected $casts = [
+        'state' => 'boolean'
+        
+     ];
+     public function post() : BelongsTo {
+        return $this->belongsTo(Post::class);
+     }
+     public function user() : BelongsTo {
+        return $this->belongsTo(User::class);
+     }
+
+}
---
+++ app/Models/Post.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Post extends Model
+{
+    // Ajouter HasFactory et importer la class
+    use HasFactory;
+    protected $fillable = [
+      'title',
+      'message',
+      'state',
+      'user_id',
+      
+   
+];
+ public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+
+    }
+    protected $casts = [
+        'state'=>'boolean'
+    ];
+
+    public function comments(): HasMany {
+        return $this->hasMany(Comment::class);
+    }
+}
